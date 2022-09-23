154780fda428e44ebc62837fc24ad96f8016cc5a
FizzGemge <108047082+FizzGemge@users.noreply.github.com>
2022-09-23 18:42:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6288296172396']
+global.ownernomer = "6288296172396"
+global.premium = ['6288296172396']
+global.packname = 'Sticker  Fizzx'
+global.author = 'Fizzx'
 global.sessionName = 'nazedev'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
